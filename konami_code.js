9b0b43198b03d46832a8e63317fd48994240e108
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-15 06:00:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
   
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+ 
+    if (key === alphabet[index]) {
+      index++;
+   
+      if (index === alphabet.length) {
+        alert("Hurray!");
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
   
+  document.body.addEventListener('keydown', onKeyDownHandler);
 
 }
